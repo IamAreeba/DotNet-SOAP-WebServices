9280b3e86951e687c90687a36574af3c5cbf8d28
Areeba Amjad <areeba.amjad2020@gmail.com>
2025-12-02 10:48:44 +0500

    Commit 01 = Added parameterized WebMethod to ASMX web service

---
+++ 01-WebService/01-WebService/CalculatorWebService.asmx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace _01_WebService
+{
+    /// <summary>
+    /// Summary description for CalculatorWebService
+    /// </summary>
+    [WebService(Namespace = "http://DotNet-areeba.com/webservice")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorWebService : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public string PrintName(string firstName, string lastName)
+        {
+            return string.Format(" Hello {0} {1} ", firstName, lastName);
+        }
+    }
+}
---
+++ 01-WebService/01-WebService/Notes.cs
@@ -0,0 +1,12 @@
+﻿
+
+// ================================ 01: Web Service Intro ====================================
+/*
+    . Web Service attribute Namespace is used to uniquely identify the web service over the internet.
+    . If the class name is same whni host that and other client creates class withe thwe same name its hard to find which service has to be invoked and use so we use the namespace property to uniquely identify that
+    . It can be a company name or website name
+    . In order to explore a method as part of webservice t the client this [WebMethod] is declared at the method level and the method should be public
+    . This [WebMethod] has some overloaded functions 
+    . proxy = Creating channel in client app that channel will communicate with web service
+    
+ */
